9a916b34d3727d548fe8f1e7224795bfc4de9488
Haileyesus Chekol <teamdevs7@gmail.com>
2024-08-31 14:27:23 +0100

    variables

---
+++ variables/var.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	var myName = "jack"
+	fmt.Println("my name is", myName, myName)
+
+	var name string = "kathy"
+	fmt.Println("name = ", name)
+
+	userName := "admin"
+	fmt.Println("admin Name = ", userName)
+
+	var sum int 
+
+	var (
+		num1 = 2
+		num2 = 3
+	)
+
+	sum = num1 + num2
+	fmt.Println(num1 ,"+", num2, "=", sum)
+
+	word1, word2, _ := "Hello","world" ,"!"
+	fmt.Println(word1, word2)
+
+}
