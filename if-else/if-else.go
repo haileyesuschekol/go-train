b046401ce856b3b50c57a113c935545228a35e1e
Haileyesus Chekol <teamdevs7@gmail.com>
2024-11-11 02:08:57 +0000

    formatting

--- if-else/if-else.go
+++ if-else/if-else.go
@@ -2,25 +2,25 @@ package main
 
 import "fmt"
 
-func average(a, b int) float32{
-return float32(a+b) /3
+func average(a, b int) float32 {
+	return float32(a+b) / 3
 }
 
-func main(){
+func main() {
 
-	a , b := 80,75
+	a, b := 80, 75
 
-	if(a>b){
+	if a > b {
 		fmt.Println("quize a")
-	}else if(b>a){
+	} else if b > a {
 		fmt.Println("quiz b")
-	}else {
+	} else {
 		fmt.Println("Equal")
 	}
 
-if average(a,b) > 50 {
-	fmt.Println("pass")
-}else{
-	fmt.Println("fail")
-}
+	if average(a, b) > 50 {
+		fmt.Println("pass")
+	} else {
+		fmt.Println("fail")
+	}
 }
--- loop/loop.go
+++ loop/loop.go
@@ -2,18 +2,17 @@ package main
 
 import "fmt"
 
-func main(){
-sum := 0
+func main() {
+	sum := 0
 
-for i:=0; i<=10; i++{
-	sum += i
-	fmt.Println("sum is ", sum)
-}
+	for i := 0; i <= 10; i++ {
+		sum += i
+		fmt.Println("sum is ", sum)
+	}
 
-for sum > 10{
-	sum -= 10
-	fmt.Println("decrement", sum)
-}
-
+	for sum > 10 {
+		sum -= 10
+		fmt.Println("decrement", sum)
+	}
 
 }
